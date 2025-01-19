6fdbf4a7fa4720253a074c46960584acf480ca78
Pedro Guimarães <pedroguimaraeshl.cads@gmail.com>
2025-01-18 22:06:48 -0300

    Fix grub missmatch

--- run_first.sh
+++ run_first.sh
@@ -1,9 +1,16 @@
 #/bin/bash
 
-if [-d venv] or [-d .venv]; then
-    echo "Virtual environment already exists"
+echo "Running run_first.sh"
+
+echo "Checking if virtual environment exists"
+if [ -d venv ] || [ -d .venv ]; then
+    echo "Enabling virtual environment"
+    source venv/bin/activate
+    echo "Sucess"
 else
     python -m venv venv
+    sleep 3
     source venv/bin/activate
+    sleep 1
     pip install -r requirements.txt
 fi
