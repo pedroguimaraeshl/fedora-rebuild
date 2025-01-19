98e8eff418a6303713c99d9b7ca32ede962707ec
Pedro Guimarães <pedroguimaraeshl.cads@gmail.com>
2025-01-18 21:33:17 -0300

    Start fedora-rebuild repository

---
+++ fedora-rebuild.py
@@ -0,0 +1,28 @@
+# Fedora Rebuild
+#
+# Python application to assist in fedora post installation
+#
+# Author: Pedro Guimarães
+# Date: 2019-06-01
+# Version: 0.1
+
+# change DNS
+
+# ASUS
+# Better fonts
+# NVIDIA
+# VSCode
+# oh-my-zsh
+# packages
+# flatpak
+
+# extensions
+# extensions config
+
+# fedora config text
+
+# Importing modules
+import configs as cfg
+
+#cfg.dnf5()
+cfg.grub_before_nvidia("fullhd")
